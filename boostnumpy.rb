0e14649f3dbc427c7fd66ec342834771ab92e1d5
Jakob van Santen <jvansanten@gmail.com>
2014-03-10 15:18:47 -0500

    New formula: boostnumpy

---
+++ boostnumpy.rb
@@ -0,0 +1,15 @@
+require "formula"
+
+class Boostnumpy < Formula
+  homepage ""
+  url "http://code.icecube.wisc.edu/svn/sandbox/mwolf/tools/BoostNumpy/branches/unversioned", :using => :svn
+  version "1.0.0"
+  sha1 "9246890d5533444d556732426ea05632cce73a1f"
+
+  depends_on "cmake" => :build
+
+  def install
+    system "cmake", ".", *std_cmake_args
+    system "make", "install"
+  end
+end
