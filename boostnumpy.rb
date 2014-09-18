0b3982b20dedad13981d3d5760c335156e25fd9f
Jakob van Santen <jvansanten@gmail.com>
2014-09-17 21:44:55 -0500

    Disable annoying logging with a hammer

--- boostnumpy.rb
+++ boostnumpy.rb
@@ -10,8 +10,29 @@ class Boostnumpy < Formula
   depends_on "numpy" => :python
   depends_on "boost" => "with-python"
 
+  patch :DATA
+
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/include/boost/numpy/detail/logging.hpp b/include/boost/numpy/detail/logging.hpp
+index 8f25e12..08f463b 100644
+--- a/include/boost/numpy/detail/logging.hpp
++++ b/include/boost/numpy/detail/logging.hpp
+@@ -45,11 +45,6 @@ log(
+         boost::numpy::detail::log(file, line, func, oss.str());                \
+     }
+ 
+-#ifdef NDEBUG
+-    #define BOOST_NUMPY_LOG(msg)
+-#else
+-    #define BOOST_NUMPY_LOG(msg) \
+-        BOOST_NUMPY_DETAIL_STREAM_LOGGER(__FILE__, __LINE__, __PRETTY_FUNCTION__, msg)
+-#endif
++#define BOOST_NUMPY_LOG(msg)
+ 
+ #endif // BOOST_NUMPY_DETAIL_LOGGING_HPP_INCLUDED
