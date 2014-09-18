f075c9f6fee5f6e32ccf2daa0999df85dfa6f43f
Jakob van Santen <jvansanten@gmail.com>
2014-09-17 20:43:12 -0500

    Update boostnumpy to 0.2

--- boostnumpy.rb
+++ boostnumpy.rb
@@ -2,13 +2,13 @@ require "formula"
 
 class Boostnumpy < Formula
   homepage "https://github.com/martwo/BoostNumpy"
-  url "http://icecube.wisc.edu/~mwolf/tools/BoostNumpy-0.1.0.tar.gz"
-  version "0.1.0"
-  sha1 "632e5f6c60e1a8c8c0ceeca0a1e4003d5d084984"
+  url "https://github.com/martwo/BoostNumpy/archive/V0.2.0.tar.gz"
+  version "0.2.0"
+  sha1 "f7a6c2661c502ad67a2e40048b99635deb8d73a8"
 
   depends_on "cmake" => :build
   depends_on "numpy" => :python
-  depends_on "boost"
+  depends_on "boost" => "with-python"
 
   def install
     system "cmake", ".", *std_cmake_args
