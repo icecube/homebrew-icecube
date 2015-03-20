c9df1b1c52267150f500e5b70558ec4f014ec2f7
Claudio Kopper <ckopper@icecube.wisc.edu>
2015-03-20 14:38:10 -0600

    add a formula for multinest (based on homebrew-nt's version including a patch to turn off most of the output to cout during normal operation if the feedback parameter is turned off)

---
+++ multinest.rb
@@ -0,0 +1,184 @@
+require "formula"
+
+class Multinest < Formula
+  homepage 'http://www.mrao.cam.ac.uk/software/multinest/'
+  url 'https://github.com/JohannesBuchner/MultiNest/archive/96249c628c1d67ccdcd8d235fd19191fdf011c42.tar.gz'
+  version '3.8'
+  sha1 '68a03cbbfe3bb29fab1fec6026196766eb306fbd'
+  head 'https://github.com/JohannesBuchner/MultiNest.git'
+
+  depends_on 'cmake' => :build
+  depends_on :fortran
+  depends_on :mpi => [ :cc, :fortran, :optional ]
+
+  patch :DATA
+
+  def install
+    ENV.deparallelize
+
+    system 'cmake', '.', *std_cmake_args
+    system 'make'
+    system 'make', 'install'
+
+    # documentation is in the form of a README
+    doc.install('README')
+
+    # the license agreement
+    doc.install('LICENCE')
+    doc.install('license.pdf')
+  end
+
+  def caveats; <<-EOS.undent
+    By installing MultiNest, you are agreeing to its licence. After the formula
+    has been installed, the README and LICENCE files can be found in #{doc}.
+    The examples are installed to #{HOMEBREW_PREFIX}/share/MultiNest.
+    EOS
+  end
+end
+
+__END__
+diff --git a/src/CMakeLists.txt b/src/CMakeLists.txt
+index 91d892c..600da86 100644
+--- a/src/CMakeLists.txt
++++ b/src/CMakeLists.txt
+@@ -179,7 +179,7 @@ INSTALL(FILES ${CMAKE_SOURCE_DIR}/CMakeModules/FindMultiNest.cmake DESTINATION
+ # and ensure the chains directory is created too
+ INSTALL(DIRECTORY
+     ${CMAKE_SOURCE_DIR}/bin/chains
+-    DESTINATION bin
++    DESTINATION share/MultiNest/examples
+ )
+ 
+ # ============================
+diff --git a/src/example_ackley/CMakeLists.txt b/src/example_ackley/CMakeLists.txt
+index c435bb9..6f50ba2 100644
+--- a/src/example_ackley/CMakeLists.txt
++++ b/src/example_ackley/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(ackley ${SOURCE})
+ target_link_libraries(ackley ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS ackley DESTINATION bin)
++INSTALL(TARGETS ackley DESTINATION share/MultiNest/examples)
+diff --git a/src/example_eggbox_C++/CMakeLists.txt b/src/example_eggbox_C++/CMakeLists.txt
+index 6e38d93..9293f0e 100644
+--- a/src/example_eggbox_C++/CMakeLists.txt
++++ b/src/example_eggbox_C++/CMakeLists.txt
+@@ -9,4 +9,4 @@ add_executable(eggboxC++ ${SOURCE})
+ target_link_libraries(eggboxC++ ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS eggboxC++ DESTINATION bin)
++INSTALL(TARGETS eggboxC++ DESTINATION share/MultiNest/examples)
+diff --git a/src/example_eggbox_C/CMakeLists.txt b/src/example_eggbox_C/CMakeLists.txt
+index 99d4e11..4295eb5 100644
+--- a/src/example_eggbox_C/CMakeLists.txt
++++ b/src/example_eggbox_C/CMakeLists.txt
+@@ -11,4 +11,4 @@ add_executable(eggboxC ${SOURCE})
+ target_link_libraries(eggboxC ${MultiNest_LIBRARIES} m)
+ 
+ # Set install directory
+-INSTALL(TARGETS eggboxC DESTINATION bin)
++INSTALL(TARGETS eggboxC DESTINATION share/MultiNest/examples)
+diff --git a/src/example_gauss_shell/CMakeLists.txt b/src/example_gauss_shell/CMakeLists.txt
+index e2faef0..bdbccf5 100644
+--- a/src/example_gauss_shell/CMakeLists.txt
++++ b/src/example_gauss_shell/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(gauss_shell ${SOURCE})
+ target_link_libraries(gauss_shell ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS gauss_shell DESTINATION bin)
++INSTALL(TARGETS gauss_shell DESTINATION share/MultiNest/examples)
+diff --git a/src/example_gaussian/CMakeLists.txt b/src/example_gaussian/CMakeLists.txt
+index 7648819..a60d71a 100644
+--- a/src/example_gaussian/CMakeLists.txt
++++ b/src/example_gaussian/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(gaussian ${SOURCE})
+ target_link_libraries(gaussian ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS gaussian DESTINATION bin)
++INSTALL(TARGETS gaussian DESTINATION share/MultiNest/examples)
+diff --git a/src/example_himmelblau/CMakeLists.txt b/src/example_himmelblau/CMakeLists.txt
+index 0ac0f18..cce939c 100644
+--- a/src/example_himmelblau/CMakeLists.txt
++++ b/src/example_himmelblau/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(himmelblau ${SOURCE})
+ target_link_libraries(himmelblau ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS himmelblau DESTINATION bin)
++INSTALL(TARGETS himmelblau DESTINATION share/MultiNest/examples)
+diff --git a/src/example_obj_detect/CMakeLists.txt b/src/example_obj_detect/CMakeLists.txt
+index 02acb13..837e08e 100644
+--- a/src/example_obj_detect/CMakeLists.txt
++++ b/src/example_obj_detect/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(obj_detect ${SOURCE})
+ target_link_libraries(obj_detect ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS obj_detect DESTINATION bin)
++INSTALL(TARGETS obj_detect DESTINATION share/MultiNest/examples)
+diff --git a/src/example_rosenbrock/CMakeLists.txt b/src/example_rosenbrock/CMakeLists.txt
+index fb3d6ef..56e1244 100644
+--- a/src/example_rosenbrock/CMakeLists.txt
++++ b/src/example_rosenbrock/CMakeLists.txt
+@@ -17,4 +17,4 @@ add_executable(rosenbrock ${SOURCE})
+ target_link_libraries(rosenbrock ${MultiNest_LIBRARIES})
+ 
+ # Set install directory
+-INSTALL(TARGETS rosenbrock DESTINATION bin)
++INSTALL(TARGETS rosenbrock DESTINATION share/MultiNest/examples)
+diff --git a/src/nested.F90 b/src/nested.F90
+index 89cefb2..134b15f 100755
+--- a/src/nested.F90
++++ b/src/nested.F90
+@@ -213,17 +213,19 @@ contains
+ 		else
+ 			resumeFlag=.false.
+ 		endif
+-      
+-		write(*,*)"*****************************************************"
+-		write(*,*)"MultiNest v3.8"
+-      		write(*,*)"Copyright Farhan Feroz & Mike Hobson"
+-      		write(*,*)"Release Oct 2014"
+-		write(*,*)
+-      		write(*,'(a,i4)')" no. of live points = ",nest_nlive
+-      		write(*,'(a,i4)')" dimensionality = ",nest_ndims
+-		if(ceff) write(*,'(a)')" running in constant efficiency mode"
+-      		if(resumeFlag) write(*,'(a)')" resuming from previous job"
+-      		write(*,*)"*****************************************************"
++
++        if (fback) then
++            write(*,*)"*****************************************************"
++            write(*,*)"MultiNest v3.8"
++            write(*,*)"Copyright Farhan Feroz & Mike Hobson"
++            write(*,*)"Release Oct 2014"
++            write(*,*)
++            write(*,'(a,i4)')" no. of live points = ",nest_nlive
++            write(*,'(a,i4)')" dimensionality = ",nest_ndims
++            if(ceff) write(*,'(a)')" running in constant efficiency mode"
++            if(resumeFlag) write(*,'(a)')" resuming from previous job"
++            write(*,*)"*****************************************************"
++        endif
+         
+ 		if (fback) write (*,*) 'Starting MultiNest'
+ 	
+@@ -364,12 +366,14 @@ contains
+ 	if( .not.bogus ) call clusteredNest(p,phyP,l,loglike,dumper,context)
+ 	
+ 	if( my_rank==0 ) then
+-		if( .not.bogus ) then
++		if (fback) then
++		    if( .not.bogus ) then
+ 			write(*,*)"ln(ev)=",gZ,"+/-",sqrt(ginfo/dble(nlive))
+ 			write(*,'(a,i12)')' Total Likelihood Evaluations: ', numlike
+ 			write(*,*)"Sampling finished. Exiting MultiNest"
+-		else
++		    else
+ 			write(*,*)"Exit signal received"
++		    endif
+ 		endif
+ 		setBlk=.false.
+ 	endif
