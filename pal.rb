fd0084cd57a73164b785095daa6cbc7e958d716c
nega <nega@icecube.umd.edu>
2015-09-01 17:36:38 -0400

    update pal to the current version and add bottle

--- pal.rb
+++ pal.rb
@@ -1,16 +1,22 @@
 require "formula"
 
 class Pal < Formula
+  bottle do
+    cellar :any
+    sha256 "ce0e56c0554480817510f655aa4ffdea97c6fa03e74b7124c33f22275ecc461a" => :el_capitan
+  end
+
   homepage "https://github.com/IceCube-SPNO/pal"
-  url "https://github.com/IceCube-SPNO/pal/archive/alpha.tar.gz"
-  version "alpha"
-  sha1 "0d0e2ec6bbf24e862f39ebe05f822d27b043f0aa"
+  url "https://github.com/IceCube-SPNO/pal/archive/master.tar.gz"
+  version "0.9.1"
+  sha1 "cad2c47efb4a0eea418fee2d1ba1a5efb75ac40e"
+  sha256 "c60e8f6cf338a47978a6c1deffaff2f8e2bd7b8493c102020a1040e5495b1df6"
 
   depends_on "erfa"
   # batteries not included
-  depends_on :autoconf => :build
-  depends_on :automake => :build
-  depends_on :libtool => :build
+  depends_on "autoconf" => :build
+  depends_on "automake" => :build
+  depends_on "libtool"  => :build
 
   def install
     system "autoreconf", "--install", "--symlink", buildpath
