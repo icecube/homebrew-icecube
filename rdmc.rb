d99db095e358079c9699136ddd8962104eb6f8e3
nega <nega@icecube.umd.edu>
2016-09-06 15:13:54 -0400

    update sha1 to sha256

--- rdmc.rb
+++ rdmc.rb
@@ -3,7 +3,7 @@ require 'formula'
 class Rdmc < Formula
   homepage 'http://code.icecube.wisc.edu/'
   url 'http://code.icecube.wisc.edu/tools/distfiles/rdmc/rdmc-2.9.5.tar.bz2'
-  sha1 '6fc3699ee3db05d8b7580ffae8e45b710b700bae'
+  sha256 '90aeb5cc5dd00a5f3182c3966bbcb8d20d2f6494b6bfda59f2231b89c37416d1'
 
   def install
     inreplace 'Makefile.in', '$(libdir)/@PACKAGE@', '$(libdir)'
