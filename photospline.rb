b56b9d00b62ecad96ea7923f502b1421db66e370
Jakob van Santen <jvansanten@gmail.com>
2017-02-13 10:35:39 +0100

    Bump photospline release

--- photospline.rb
+++ photospline.rb
@@ -1,9 +1,9 @@
 class Photospline < Formula
   desc "Fit and evaluate tensor-product B-spline surfaces"
   homepage "https://github.com/cnweaver/photospline"
-  version "2.0.0-alpha.1"
-  url "https://github.com/cnweaver/photospline/archive/v2.0.0-alpha.1.tar.gz"
-  sha256 "f4c8329f05299a7128d7936bb9abd2bea9d857566b56c58621efaff68976a4d7"
+  version "2.0.0-alpha.2"
+  url "https://github.com/cnweaver/photospline/archive/v2.0.0-alpha.2.tar.gz"
+  sha256 "6240f32caaae7c2a83b80669665cb84b5c0bb308b3908fa2c835aeb9e893cc76"
   head "https://github.com/cnweaver/photospline.git"
 
   depends_on "cmake" => :build
