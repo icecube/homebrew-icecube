0a070847aa10df12ccccd5f0dad70227da93248d
nega <nega0@users.noreply.github.com>
2018-01-24 16:26:16 -0500

    update formulae with new bottles and source versions

--- erfa.rb
+++ erfa.rb
@@ -5,11 +5,12 @@ class Erfa < Formula
     root_url "http://code.icecube.wisc.edu/tools/bottles/"
     cellar :any
     sha256 "775917084ad3c9306e3b194ba58833563d169937da7f3e7e277b04fd02a3df83" => :sierra
+    sha256 "d5959442ced505d9cf4cb73265c486f12e09c59e9228e18198cfcec50acf456c" => :high_sierra
   end
 
   homepage "https://github.com/liberfa/erfa"
-  url "https://github.com/liberfa/erfa/releases/download/v1.2.0/erfa-1.2.0.tar.gz"
-  sha256 "361bc9c5f783140aa337eba8379476ec45ec13627a8c839ccc1aaec104ad3316"
+  url "https://github.com/liberfa/erfa/releases/download/v1.4.0/erfa-1.4.0.tar.gz"
+  sha256 "035b7f0ad05c1191b8588191ba4b19ba0f31afa57ad561d33bd5417d9f23e460"
 
   def install
     system "./configure", "--disable-debug",
--- pal.rb
+++ pal.rb
@@ -4,14 +4,15 @@ class Pal < Formula
   bottle do
     root_url "http://code.icecube.wisc.edu/tools/bottles/"
     cellar :any
-    rebuild 1
     sha256 "dc8d557cb13acdcfbf5d8db0f121b962e4b9b02aca6802c857c6fe11a47a3a82" => :sierra
     sha256 "ce0e56c0554480817510f655aa4ffdea97c6fa03e74b7124c33f22275ecc461a" => :el_capitan
+    sha256 "588d8843a34f04a6d8b20757084f7282c49c34b41ad038ac073c64827cf0da2d" => :high_sierra
   end
 
   homepage "https://github.com/IceCube-SPNO/pal"
-  url "https://github.com/IceCube-SPNO/pal/archive/v0.9.1.tar.gz"
-  sha256 "c60e8f6cf338a47978a6c1deffaff2f8e2bd7b8493c102020a1040e5495b1df6"
+  version "0.9.7"
+  url "https://github.com/IceCube-SPNO/pal/archive/master.tar.gz"
+  sha256 "001c4225d90a6887e8a7da45d1db2147bfa3c8ca3545492b20594042f9fcdabe"
 
   depends_on "erfa"
   # batteries not included
