6473dd765ffd71748ed70583c6a6afe7fe603e73
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-03-10 13:46:16 -0600

    update to cppzmq 4.7.1

--- cppzmq.rb
+++ cppzmq.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class Cppzmq < Formula
   homepage 'https://github.com/zeromq/cppzmq'
-  url 'https://github.com/zeromq/cppzmq/archive/v4.3.0.tar.gz'
-  sha256 '27d1f56406ba94ee779e639203218820975cf68174f92fbeae0f645df0fcada4'
-  version '4.3.0'
+  url 'https://github.com/zeromq/cppzmq/archive/v4.7.1.tar.gz'
+  sha256 '9853e0437d834cbed5d3c223bf1d755cadee70e7c964c6e42c4c6783dee5d02c'
+  version '4.7.1'
 
   depends_on 'zeromq'
   depends_on 'cmake' => :build
 
   def install
-    system "cmake", ".", *std_cmake_args
+    system "cmake", ".", "-DCPPZMQ_BUILD_TESTS=OFF", *std_cmake_args
     system "make", "install"
   end
 
