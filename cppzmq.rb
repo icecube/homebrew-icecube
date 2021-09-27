0095003ca32144577f504d0044c9c341f002c022
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-09-27 10:31:15 -0500

    Upgrade cppzmq to 1.8.1

--- cppzmq.rb
+++ cppzmq.rb
@@ -2,8 +2,8 @@ require 'formula'
 
 class Cppzmq < Formula
   homepage 'https://github.com/zeromq/cppzmq'
-  url 'https://github.com/zeromq/cppzmq/archive/v4.7.1.tar.gz'
-  sha256 '9853e0437d834cbed5d3c223bf1d755cadee70e7c964c6e42c4c6783dee5d02c'
+  url 'https://github.com/zeromq/cppzmq/archive/v4.8.1.tar.gz'
+  sha256 '7a23639a45f3a0049e11a188e29aaedd10b2f4845f0000cf3e22d6774ebde0af'
   version '4.7.1'
 
   depends_on 'zeromq'
