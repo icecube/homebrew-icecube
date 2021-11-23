f09edb574028dd0bcdc8953112c7b3eea0803960
Kevin Meagher <11620178+kjmeagher@users.noreply.github.com>
2021-11-23 11:55:36 -0600

    add numpy dep to photospline

--- photospline.rb
+++ photospline.rb
@@ -1,13 +1,13 @@
 class Photospline < Formula
   desc "Fit and evaluate tensor-product B-spline surfaces"
-  homepage "https://github.com/cnweaver/photospline"
-  url "https://github.com/cnweaver/photospline/archive/v2.0.7.tar.gz"
+  homepage "https://github.com/icecube/photospline"
+  url "https://github.com/icecube/photospline/archive/v2.0.7.tar.gz"
   sha256 "59a3607c4aa036c55bcd233e8a0ec11575bd74173f3b4095cc6a77aa50baebcd"
-  head "https://github.com/cnweaver/photospline.git"
 
   depends_on "cmake" => :build
   depends_on "cfitsio"
   depends_on "suite-sparse"
+  depends_on "numpy"
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -16,6 +16,6 @@ class Photospline < Formula
 
   test do
     # just make sure it's linked correctly
-    system "python", "-c", "import photospline"
+    system "python3", "-c", "import photospline"
   end
 end
