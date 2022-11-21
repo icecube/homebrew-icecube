35772214fa92789ed8404da7c8667d886e97e41b
nega <nega0@users.noreply.github.com>
2022-11-21 17:35:12 -0500

    make brew audit happier
    Includes setting MACOSX_DEPLOYMENT_TARGET to the current os version.
    This helps stop libtool from being dumb and setting a flat namespace

--- Formula/pal.rb
+++ Formula/pal.rb
@@ -12,7 +12,6 @@ class Pal < Formula
   desc "Positional Astronomy Library"
   homepage "https://github.com/Starlink/pal"
   url "https://github.com/Starlink/pal/releases/download/v0.9.8/pal-0.9.8.tar.gz"
-  version "0.9.8"
   sha256 "191fde8c4f45d6807d4b011511344014966bb46e44029a4481d070cd5e7cc697"
 
   depends_on "autoconf" => :build
@@ -21,6 +20,8 @@ class Pal < Formula
   depends_on "erfa"
 
   def install
+    ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version.to_s
+
     system "./configure", "--disable-debug", "--without-starlink",
                           "--disable-dependency-tracking",
                           "--disable-silent-rules",
