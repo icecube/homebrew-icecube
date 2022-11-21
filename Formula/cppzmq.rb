5171cccf7087c103f4a994957fa71963e276782b
nega <nega0@users.noreply.github.com>
2022-11-21 16:18:31 -0500

    make brew audit happy

--- Formula/cppzmq.rb
+++ Formula/cppzmq.rb
@@ -1,17 +1,19 @@
-require 'formula'
-
 class Cppzmq < Formula
-  homepage 'https://github.com/zeromq/cppzmq'
-  url 'https://github.com/zeromq/cppzmq/archive/v4.8.1.tar.gz'
-  sha256 '7a23639a45f3a0049e11a188e29aaedd10b2f4845f0000cf3e22d6774ebde0af'
-  version '4.7.1'
+  desc "Header-only C++ binding for libzmq"
+  homepage "https://github.com/zeromq/cppzmq"
+  url "https://github.com/zeromq/cppzmq/archive/v4.8.1.tar.gz"
+  version "4.7.1"
+  sha256 "7a23639a45f3a0049e11a188e29aaedd10b2f4845f0000cf3e22d6774ebde0af"
 
-  depends_on 'zeromq'
-  depends_on 'cmake' => :build
+  depends_on "cmake" => :build
+  depends_on "zeromq"
 
   def install
     system "cmake", ".", "-DCPPZMQ_BUILD_TESTS=OFF", *std_cmake_args
     system "make", "install"
   end
 
+  test do
+    system "true"
+  end
 end
