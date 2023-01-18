5c1f64b92114d64cfcb67d6e89cb2ade86aab8c1
nega <nega0@users.noreply.github.com>
2023-01-18 14:00:51 -0500

    patch to update version detection for SuiteSparse

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -10,6 +10,11 @@ class Photospline < Formula
   depends_on "python@3.11"
   depends_on "suite-sparse"
 
+  stable do
+    # update SuiteSparse version detection through photospline v2.1.1
+    patch :DATA
+  end
+
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
@@ -20,3 +25,17 @@ class Photospline < Formula
     system "python3", "-c", "import photospline"
   end
 end
+
+__END__
+diff --git a/cmake/Packages/SuiteSparse.cmake b/cmake/Packages/SuiteSparse.cmake
+index 8d9e7a2..35e14b9 100644
+--- a/cmake/Packages/SuiteSparse.cmake
++++ b/cmake/Packages/SuiteSparse.cmake
+@@ -47,7 +47,7 @@ IF (NOT SUITESPARSE_FOUND)
+     IF (EXISTS "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h")
+       FILE (READ "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h" _SUITESPARSE_VERSION)
+     ENDIF (EXISTS "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h")
+-    STRING (REGEX REPLACE ".*define SUITESPARSE_MAIN_VERSION ([0-9]+).*" "\\1"
++    STRING (REGEX REPLACE ".*define SUITESPARSE_MAIN_VERSION +([0-9]+).*" "\\1"
+       SUITESPARSE_VERSION "${_SUITESPARSE_VERSION}")
+   ENDIF (SUITESPARSE_INCLUDE_DIR)
