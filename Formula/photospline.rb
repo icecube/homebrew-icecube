6a52a5a4b37b4dad550904fcc64f3cd20a37b06c
nega <nega0@users.noreply.github.com>
2023-01-18 14:10:22 -0500

    update photospline to v2.1.1

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -1,19 +1,14 @@
 class Photospline < Formula
   desc "Fit and evaluate tensor-product B-spline surfaces"
   homepage "https://github.com/icecube/photospline"
-  url "https://github.com/icecube/photospline/archive/v2.1.0.tar.gz"
-  sha256 "bd6c58df8893917909b79ef2510a2043f909fbb7020bdace328d4d36e0222b60"
+  url "https://github.com/icecube/photospline/archive/v2.1.1.tar.gz"
+  sha256 "0a0dae8e1b994a35be23896982bd572fa97c617ad55a99b3da34782ad9435de8"
 
   depends_on "cmake" => :build
   depends_on "cfitsio"
   depends_on "numpy"
   depends_on "python@3.11"
   depends_on "suite-sparse"
-
-  stable do
-    # update SuiteSparse version detection through photospline v2.1.1
-    patch :DATA
-  end
 
   def install
     system "cmake", ".", *std_cmake_args
@@ -25,17 +20,3 @@ class Photospline < Formula
     system "python3", "-c", "import photospline"
   end
 end
-
-__END__
-diff --git a/cmake/Packages/SuiteSparse.cmake b/cmake/Packages/SuiteSparse.cmake
-index 8d9e7a2..35e14b9 100644
---- a/cmake/Packages/SuiteSparse.cmake
-+++ b/cmake/Packages/SuiteSparse.cmake
-@@ -47,7 +47,7 @@ IF (NOT SUITESPARSE_FOUND)
-     IF (EXISTS "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h")
-       FILE (READ "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h" _SUITESPARSE_VERSION)
-     ENDIF (EXISTS "${SUITESPARSE_INCLUDE_DIR}/UFconfig.h")
--    STRING (REGEX REPLACE ".*define SUITESPARSE_MAIN_VERSION ([0-9]+).*" "\\1"
-+    STRING (REGEX REPLACE ".*define SUITESPARSE_MAIN_VERSION +([0-9]+).*" "\\1"
-       SUITESPARSE_VERSION "${_SUITESPARSE_VERSION}")
-   ENDIF (SUITESPARSE_INCLUDE_DIR)
