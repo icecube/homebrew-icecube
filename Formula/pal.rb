e5182c1b0c3b4b150ea17c739264b8e50db0190a
nega <nega0@users.noreply.github.com>
2023-04-12 18:09:42 -0400

    patch around inclusion error on macOS

--- Formula/pal.rb
+++ Formula/pal.rb
@@ -24,6 +24,7 @@ class Pal < Formula
     depends_on "libtool"  => :build
   end
 
+  patch :DATA
   def install
     system "./configure", "--disable-debug", "--without-starlink",
                           "--disable-dependency-tracking",
@@ -33,3 +34,33 @@ class Pal < Formula
   end
 
 end
+__END__
+diff -ur pal-0.9.8.old/palDfltin.c pal-0.9.8/palDfltin.c
+--- pal-0.9.8.old/palDfltin.c	2018-06-28 18:52:17.000000000 -0400
++++ pal-0.9.8/palDfltin.c	2023-04-12 17:34:31.000000000 -0400
+@@ -105,6 +105,12 @@
+ #endif
+ #endif
+ 
++/* System include files */
++#include <stdlib.h>
++#include <string.h>
++#include <errno.h>
++#include <ctype.h>
++
+ /* isblank() is a C99 feature so we just reimplement it if it is missing */
+ #if HAVE_ISBLANK
+ #define _POSIX_C_SOURCE 200112L
+@@ -123,12 +129,6 @@
+ #include <bsd/string.h>
+ #endif
+ 
+-/* System include files */
+-#include <stdlib.h>
+-#include <string.h>
+-#include <errno.h>
+-#include <ctype.h>
+-
+ #include "pal.h"
+ 
+ #if HAVE_COPYSIGN
