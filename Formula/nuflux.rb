9b0b10869dc9899cc0525b5497f6e7effba36758
nega <nega0@users.noreply.github.com>
2023-08-09 23:18:21 -0400

    update nuflux formula

--- Formula/nuflux.rb
+++ Formula/nuflux.rb
@@ -1,21 +1,22 @@
 class Nuflux < Formula
-  desc "A library for calculating atmospheric neutrino fluxes."
+  desc "Library for calculating atmospheric neutrino fluxes"
   homepage "https://docs.icecube.aq/nuflux/main"
   url "https://github.com/icecube/nuflux/archive/refs/tags/v2.0.3.tar.gz"
   sha256 "b7a3c88107c73b81bf021acd5e6e69f9646811cc041b1dc14fffbdffcd736981"
-  license "LGPL-3.0"
+  license "LGPL-3.0-or-later"
 
-  depends_on "boost-python3"
-  depends_on "numpy"
-  depends_on "cfitsio"
-  depends_on "icecube/icecube/photospline"
-  depends_on "pkg-config" => :build
+  depends_on "cmake" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
+  depends_on "pkg-config" => :build
+  depends_on "boost-python3"
+  depends_on "cfitsio"
+  depends_on "icecube/icecube/photospline"
+  depends_on "numpy"
 
   def install
+    inreplace "meson_options.txt", "value : 'True'", "value : true"
     mkdir "build" do
-      ENV["PATH"] +=":/usr/local/bin"
       system "meson", *std_meson_args, ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
