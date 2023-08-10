b7782f01a292cb58fa0e0788c2513525924e8eb4
nega <nega0@users.noreply.github.com>
2023-08-09 23:13:40 -0400

    clean up erfa formula
     - remove old bottle block
     - add test block
     - style and audit checks pass

--- Formula/erfa.rb
+++ Formula/erfa.rb
@@ -1,15 +1,5 @@
-require "formula"
-
 class Erfa < Formula
-  #bottle do
-  #  root_url "http://code.icecube.wisc.edu/tools/bottles"
-  #  cellar :any
-  #  sha256 "775917084ad3c9306e3b194ba58833563d169937da7f3e7e277b04fd02a3df83" => :sierra
-  #  sha256 "d5959442ced505d9cf4cb73265c486f12e09c59e9228e18198cfcec50acf456c" => :high_sierra
-  #  rebuild 1
-  #  sha256 "24bfd86d7f969554c6112566e2ff4f0450e899719ee61e62f14792ff7a2fbd71" => :mojave
-  #end
-
+  desc "Essential Routines for Fundamental Astronomy"
   homepage "https://github.com/liberfa/erfa"
   url "https://github.com/liberfa/erfa/releases/download/v1.7.2/erfa-1.7.2.tar.gz"
   sha256 "b85ee6f723c8c01a7da85982a5f53838e8133f65ebf98ddf70a454e95cbaf96e"
@@ -22,5 +12,24 @@ class Erfa < Formula
     system "make"
     system "make", "check"
     system "make", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOF
+      #include <erfaextra.h>
+
+      int main (int argc, char *argv[]) {
+        eraVersion();
+        return 0;
+      }
+    EOF
+
+    flags = %W[
+      -I#{include}
+      -L#{lib}
+      -lerfa
+    ]
+    system ENV.cxx, "test.cpp", "-o", "test", *flags
+    system "./test"
   end
 end
