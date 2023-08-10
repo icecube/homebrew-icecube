4a0ac659fc7883212c94ea101619abd8b0966b59
nega <nega0@users.noreply.github.com>
2023-08-10 12:01:52 -0400

    let homebrew install the nuflux python extension
    Closes #41.
    
    Signed-off-by: nega <240504+nega0@users.noreply.github.com>

--- Formula/nuflux.rb
+++ Formula/nuflux.rb
@@ -13,17 +13,50 @@ class Nuflux < Formula
   depends_on "cfitsio"
   depends_on "icecube/icecube/photospline"
   depends_on "numpy"
+  depends_on "python@3.11"
+
+  patch :DATA
+
+  def python3
+    "python3.11"
+  end
 
   def install
     inreplace "meson_options.txt", "value : 'True'", "value : true"
+    inreplace "meson.build", "c++11", "c++14" # numpy wants 14 now
     mkdir "build" do
       system "meson", *std_meson_args, ".."
       system "ninja", "-v"
       system "ninja", "install", "-v"
     end
+
+    system python3, *Language::Python.setup_install_args(prefix, python3)
   end
 
   test do
     system "python3", "-c", "assert __import__('nuflux').makeFlux('H3a_SIBYLL21').getFlux(14,1e5,0)>0"
   end
 end
+
+__END__
+diff --git a/meson.build b/meson.build
+index 10a384a..38de592 100644
+--- a/meson.build
++++ b/meson.build
+@@ -57,6 +57,7 @@ cppcomp = meson.get_compiler('cpp')
+ pymod = import('python')
+ python = pymod.find_installation(get_option('python'),required: false)
+ 
++if false
+ if not python.found()
+   warning('Can\'t find python: "'+get_option('python')+'", skipping python build')
+ else
+@@ -100,5 +101,7 @@ else
+   endif
+ 
+ endif
++endif
+ 
++test('test_fluxes',find_program('tests/test_fluxes.py'))
+ subdir('docs')
+
