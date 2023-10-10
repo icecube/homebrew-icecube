9fb93b20b2e641f1bdb06f08a6a6cd82ed72438a
nega <nega0@users.noreply.github.com>
2023-10-10 14:56:30 -0400

    disable photospline bottles

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -5,15 +5,15 @@ class Photospline < Formula
   sha256 "81f79b42fd63e12c13cc369fb5c6ef356389f7c7aaa10a584aae2e22dba79ccf"
   revision 1
 
-  bottle do
-    root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.1.1_1"
-    sha256 cellar: :any, arm64_ventura:  "4183c720d486d8a29e77006c8a85049513ab96ebb7b2955e0636a4694303d5d9"
-    sha256 cellar: :any, arm64_monterey: "16d50cde84e0b33eb790e3682c43b577963439feae1dca3cb7a771a74f4d94da"
-    sha256 cellar: :any, ventura:        "8082c0a51d43dede4563599340dba960813bda7237619e36d390c54a589ab4da"
-    sha256 cellar: :any, monterey:       "08e1586ed1a2331cd71497050fb3addbcf1a04b447c1da52cd254763d6817ffd"
-    sha256 cellar: :any, big_sur:        "a6689193e73e25df4a332f3742c57e924d026f89ad915b808f46eeee02fcbac2"
-  end
-
+  # bottle do
+  #   root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.1.1_1"
+  #   sha256 cellar: :any, arm64_ventura:  "4183c720d486d8a29e77006c8a85049513ab96ebb7b2955e0636a4694303d5d9"
+  #   sha256 cellar: :any, arm64_monterey: "16d50cde84e0b33eb790e3682c43b577963439feae1dca3cb7a771a74f4d94da"
+  #   sha256 cellar: :any, ventura:        "8082c0a51d43dede4563599340dba960813bda7237619e36d390c54a589ab4da"
+  #   sha256 cellar: :any, monterey:       "08e1586ed1a2331cd71497050fb3addbcf1a04b447c1da52cd254763d6817ffd"
+  #   sha256 cellar: :any, big_sur:        "a6689193e73e25df4a332f3742c57e924d026f89ad915b808f46eeee02fcbac2"
+  # end
+  #
   depends_on "cmake" => :build
   depends_on "cfitsio"
   depends_on "numpy"
