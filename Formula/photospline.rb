25c85393f2f5470e3e1e4f727339b0cdd188ae07
nega <nega0@users.noreply.github.com>
2025-06-02 18:03:25 -0400

    update dependencies

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -5,13 +5,16 @@ class Photospline < Formula
   sha256 "c8bfd2a087300f3f217cecfe3e4354be4e2a485dfc503420c8ebbffeec5adf03"
 
   bottle do
-    root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.3.1_1"
-    sha256 cellar: :any, arm64_sonoma: "65883e71d1ea9905616c4033d3d8cf2ce67ea7e62652466d11a2bef1741aa85b"
+    root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.4.1"
+    sha256 cellar: :any, sonoma:  "33e36eafc6b5f8288c4c5920d1b9a6df2df07bb7ca0e5fef339e5eb2665fae9c"
+    sha256 cellar: :any, sequoia: "504a5da0876defcedad0e8d3f6f588584e82609a930e89ecef9a549b1dd2ba21"
   end
 
   depends_on "cmake" => :build
   depends_on "cfitsio"
+  depends_on "metis"
   depends_on "numpy"
+  depends_on "openblas"
   depends_on "python@3.13"
   depends_on "suite-sparse"
 
