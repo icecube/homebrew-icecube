fd858e13b992d6ed4a17c317bedb11a799759b2b
nega <nega0@users.noreply.github.com>
2025-11-10 23:24:49 -0500

    update photospline
    - add HEAD url
    - add patch for stricter clang
    - bump python

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -3,7 +3,7 @@ class Photospline < Formula
   homepage "https://github.com/icecube/photospline"
   url "https://github.com/icecube/photospline/archive/refs/tags/v2.4.1.tar.gz"
   sha256 "c8bfd2a087300f3f217cecfe3e4354be4e2a485dfc503420c8ebbffeec5adf03"
-  revision 1
+  revision 2
 
   bottle do
     root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.4.1_1"
@@ -11,10 +11,12 @@ class Photospline < Formula
     sha256 cellar: :any, arm64_sonoma:  "0a7e9f36338c32bd4bbedd40125d999441ac5fbb45330dcc2dc925cdafbd17e9"
   end
 
+  head "https://github.com/icecube/photospline.git", branch: "master"
+
   depends_on "cmake" => :build
   depends_on "cfitsio"
   depends_on "numpy"
-  depends_on "python@3.13"
+  depends_on "python@3.14"
   depends_on "suite-sparse"
 
   def install
@@ -27,3 +29,26 @@ class Photospline < Formula
     system Formula["python@3.13"].opt_bin/"python3", "-c", "import photospline"
   end
 end
+
+# remove w/ next version after 2.4.1
+__END__
+diff --git a/include/photospline/detail/splineutil.h b/include/photospline/detail/splineutil.h
+index bd7b985..828cbdc 100644
+--- a/include/photospline/detail/splineutil.h
++++ b/include/photospline/detail/splineutil.h
+@@ -2,12 +2,12 @@
+ #ifndef PHOTOSPLINE_CFITTER_SPLINEUTIL_H
+ #define PHOTOSPLINE_CFITTER_SPLINEUTIL_H
+ 
++#include <cholmod.h>
++
+ #ifdef __cplusplus
+ extern "C" {
+ #endif
+ 
+-#include <cholmod.h>
+-
+ struct ndsparse {
+ 	/*
+ 	 * This is an ntuple, and is similar to the CHOLMOD triplet
+
