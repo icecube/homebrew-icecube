6e58e2f223d02d3ebe187be88a6af779f61dc8c4
nega <nega0@users.noreply.github.com>
2026-02-16 13:10:18 -0500

    try and make brew audit happy
    Closes #66.
    
    Signed-off-by: nega <240504+nega0@users.noreply.github.com>

--- Formula/photospline.rb
+++ Formula/photospline.rb
@@ -3,6 +3,7 @@ class Photospline < Formula
   homepage "https://github.com/icecube/photospline"
   url "https://github.com/icecube/photospline/archive/refs/tags/v2.4.2.tar.gz"
   sha256 "b3873fa475434b6ff3e1013a638d5a04970d21ced5ef32af5dd6a6f4fbb79b0c"
+  head "https://github.com/icecube/photospline.git", branch: "master"
 
   bottle do
     root_url "https://github.com/icecube/homebrew-icecube/releases/download/photospline-2.4.1_1"
@@ -10,14 +11,12 @@ class Photospline < Formula
     sha256 cellar: :any, arm64_sonoma:  "0a7e9f36338c32bd4bbedd40125d999441ac5fbb45330dcc2dc925cdafbd17e9"
   end
 
-  head "https://github.com/icecube/photospline.git", branch: "master"
-
   depends_on "cmake" => :build
   depends_on "cfitsio"
   depends_on "numpy"
+  depends_on "openblas"
   depends_on "python@3.14"
   depends_on "suite-sparse"
-  depends_on "openblas"
 
   def install
     system "cmake", ".", *std_cmake_args
